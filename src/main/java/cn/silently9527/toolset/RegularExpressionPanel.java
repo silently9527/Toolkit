41d34667665750c04efe087f48f6996b0e23d37e
Wu Tian Qiang <380303318@qq.com>
2021-01-26 22:55:56 +0800

    DEV-DRIVEN complete regular match

--- src/main/java/cn/silently9527/toolset/RegularExpressionPanel.java
+++ src/main/java/cn/silently9527/toolset/RegularExpressionPanel.java
@@ -52,15 +52,19 @@ public class RegularExpressionPanel extends AbstractPanel {
 
     private void initJBList() {
         model.addElement(new RegularExample("匹配邮箱", "\\w[-\\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\\.)+[A-Za-z]{2,14}"));
+        model.addElement(new RegularExample("匹配手机号", "(13\\d|14[579]|15[^4\\D]|17[^49\\D]|18\\d)\\d{8}"));
+        model.addElement(new RegularExample("匹配IP（IPV4）", "\\d{0,3}\\.\\d{0,3}\\.\\d{0,3}\\.\\d{0,3}"));
         model.addElement(new RegularExample("匹配中文", "[\\u4e00-\\u9fa5]+"));
         model.addElement(new RegularExample("匹配双字节字符（包含汉字）", "[^\\x00-\\xff]+"));
         model.addElement(new RegularExample("匹配时间（时:分:秒）", "([01]?\\d|2[0-3]):[0-5]?\\d:[0-5]?\\d"));
-        model.addElement(new RegularExample("匹配IP（IPV4）", "\\d{0,3}\\.\\d{0,3}\\.\\d{0,3}\\.\\d{0,3}"));
         model.addElement(new RegularExample("匹配身份证", "\\d{17}[0-9Xx]|\\d{15}"));
         model.addElement(new RegularExample("匹配日期（年-月-日）", "(([0-9]{3}[1-9]|[0-9]{2}[1-9][0-9]{1}|[0-9]{1}[1-9][0-9]{2}|[1-9][0-9]{3})-(((0[13578]|1[02])-(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)-(0[1-9]|[12][0-9]|30))|(02-(0[1-9]|[1][0-9]|2[0-8]))))|((([0-9]{2})(0[48]|[2468][048]|[13579][26])|((0[48]|[2468][048]|[3579][26])00))-02-29)"));
         model.addElement(new RegularExample("匹配正整数", "[1-9]\\d*"));
         model.addElement(new RegularExample("匹配负整数", "-[1-9]\\d*"));
-        model.addElement(new RegularExample("匹配手机号", "(13\\d|14[579]|15[^4\\D]|17[^49\\D]|18\\d)\\d{8}"));
+        model.addElement(new RegularExample("空白行", "\\s"));
+        model.addElement(new RegularExample("腾讯QQ", "[1-9]([0-9]{5,11})"));
+        model.addElement(new RegularExample("邮政编码", "\\d{6}"));
+
 
         regularExampleList.setBorder(JBUI.Borders.customLine(JBColor.border(), 1));
         regularExampleList.setCellRenderer(new ColoredListCellRenderer<RegularExample>() {
